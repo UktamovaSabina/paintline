3208c1231c7f9878ef4d531ab6e8b54772bf0ee6
UktamovaSabina <sabinauktamova12052002@gmail.com>
2024-01-07 16:56:58 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const burgerIcon = document.getElementById('burger-icon');
+    const navbarList = document.getElementById('navbar-list');
+    const mediaQuery = window.matchMedia('(max-width: 700px)');
+
+    burgerIcon.addEventListener('click', function () {
+        console.log('burger clicked');
+        navbarList.style.display = 'flex'
+    });
+
+    navbarList.addEventListener("click", () => {
+        if (mediaQuery.matches) {
+            navbarList.style.display = 'none'
+        }
+    })
+
+    mediaQuery.addEventListener('change', () => {
+        if (!mediaQuery.matches) {
+            navbarList.style.display = 'flex'
+        } else {
+            navbarList.style.display = 'none'
+        }
+    })
+});
