a60f49b4b4ef66bed5b4029c09db086ae5feadd9
UktamovaSabina <sabinauktamova12052002@gmail.com>
2024-01-15 02:18:39 +0500

    changed language and fixed

---
+++ js/database.js
@@ -0,0 +1,18 @@
+const data = [
+    {
+        name: "Sabina",
+        age: 21
+    },
+    {
+        name: "Diora",
+        age: 21
+    },
+    {
+        name: "Chanela",
+        age: 21
+    },
+    {
+        name: "Valentina",
+        age: 21
+    },
+]
