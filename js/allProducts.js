018e7f17613acdc97949c0c41662aaeae6318ca0
UktamovaSabina <sabinauktamova12052002@gmail.com>
2024-01-18 20:01:21 +0500

    card style changed

--- js/allProducts.js
+++ js/allProducts.js
@@ -2,16 +2,15 @@ const allProducts = document.getElementById('all-products');
 allProducts.innerHTML = '';
 
 data.map(d => {
-    const { id, ru_name, en_name, descriptions, img, price } = d;
+    const { id, ru_name, en_name, descriptions, img } = d;
     const heading = `${ru_name} | ${en_name}`
-    return allProducts.innerHTML += `
-<li>
-    <img src="${img[0]}" alt="${ru_name}, ${en_name}" class="product-img">
+    return allProducts.innerHTML += `<li>
+    <div>
+        <img src="${img[0]}" alt="${ru_name}, ${en_name}" class="product-img">
         <div class="product-description">
-            <div class="product-description__heading">
-                <h3>${shortenProductDescription(null, 40, heading)}</h3>
-            </div>
+            <h3>${heading}</h3>
             <p>${shortenProductDescription(null, 100, descriptions[0])}</p>
-            <a href="./description.html#${id}" class="product-button">see more</a>
         </div>
+    </div>
+        <a href="./description.html#${id}" class="product-button">see more</a>
 </li>`});
