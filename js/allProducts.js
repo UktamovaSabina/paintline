836d868daef427e2b156bacd12c46581349dd1b8
UktamovaSabina <sabinauktamova12052002@gmail.com>
2024-01-21 20:29:56 +0500

    next and prev buttons added

--- js/allProducts.js
+++ js/allProducts.js
@@ -5,12 +5,14 @@ data.map(d => {
     const { id, ru_name, en_name, descriptions, img } = d;
     const heading = `${ru_name} | ${en_name}`
     return allProducts.innerHTML += `<li>
-    <div>
-        <img src="${img[0]}" alt="${ru_name}, ${en_name}" class="product-img">
-        <div class="product-description">
-            <h3>${heading}</h3>
-            <p>${shortenProductDescription(null, 100, descriptions[0])}</p>
+    <a href="./description.html#${id}" class="product-link-wrapper">
+        <div>
+            <img src="${img[0]}" alt="${ru_name}, ${en_name}" class="product-img">
+            <div class="product-description">
+                <h3>${heading}</h3>
+                <p>${shortenProductDescription(null, 100, descriptions[0])}</p>
+            </div>
         </div>
-    </div>
         <a href="./description.html#${id}" class="product-button">see more</a>
+    </a>
 </li>`});
--- js/singleProduct.js
+++ js/singleProduct.js
@@ -2,7 +2,11 @@ const productID = Number(window.location.hash.split("#").join(''));
 const singleProductContainer = document.getElementById("single-product__container");
 const { id, ru_name, en_name, img, descriptions, made_in } = data[productID - 1];
 const scaleImgWrapper = document.getElementById("scale-img-wrapper");
+const dataImgLength = img.length;
+let scaledImg;
+let showImgIndex;
 
+// single product description
 singleProductContainer.innerHTML = `
 <div class="single-product__description">
     <h2><strong>${ru_name} | </strong> <span>${en_name}</span></h2>
@@ -18,26 +22,55 @@ singleProductContainer.innerHTML = `
 </div>
 <div class="single-product__imgs-wrapper" id="single-product__imgs-wrapper"></div>`;
 
-const descriptionList = document.getElementById("description__list");
-const singleImgsWrapper = document.getElementById("single-product__imgs-wrapper");
+let descriptionList;
+let singleImgsWrapper;
+
+// single product all paragraphs and images
 document.addEventListener("DOMContentLoaded", function () {
+    descriptionList = document.getElementById("description__list");
+    singleImgsWrapper = document.getElementById("single-product__imgs-wrapper");
     descriptions.map(d => {
         descriptionList.innerHTML += `<p>${d}</p>`
     });
-    img.map(i => {
-        singleImgsWrapper.innerHTML += `<img src="${i}" alt="${ru_name}, ${en_name}" class="single-product-img" onclick="showImage({src:'${i}', alt: '${ru_name}, ${en_name}'})"></img>`
+    img.map((i, index) => {
+        singleImgsWrapper.innerHTML += `
+        <img src="${i}" alt="${ru_name}, ${en_name}" class="single-product-img" onclick="showImage({src:'${i}', alt: '${ru_name}, ${en_name}', id: ${index}})"></img>`
     })
 });
 
+// scaling clicked image
 function showImage(obj) {
+    showImgIndex = obj.id;
     scaleImgWrapper.style.display = "flex";
-    scaleImgWrapper.innerHTML = `<img src=${obj.src} alt=${obj.alt}>`;
-    scaleImgWrapper.innerHTML += `<button type="button" class="close-imgs-wrapper" onclick="closeImage()">close</button>`
+    scaleImgWrapper.innerHTML = `<span class="prev-btn" onclick="showPrevImg()">prev</span><img src="${obj.src}" alt="${obj.alt}" id="scaled-img"><span class="next-btn"  onclick="showNextImg()">next</span>`;
+    scaleImgWrapper.innerHTML += `<button type="button" class="close-imgs-wrapper" onclick="closeImage()">close</button>`;
+    scaledImg = document.getElementById("scaled-img");
 }
 
+// close scaled image
 function closeImage() {
     scaleImgWrapper.style.display = "none";
-}
+};
+
+
+function showPrevImg() {
+    if (showImgIndex === 0) {
+        showImgIndex = (img.length - 1);
+    } else {
+        showImgIndex -= 1;
+    }
+    scaledImg.src = img[showImgIndex]
+};
+
+function showNextImg() {
+    if ((dataImgLength - 1) === showImgIndex) {
+        showImgIndex = 0;
+    } else {
+        showImgIndex += 1;
+    }
+    scaledImg.src = img[showImgIndex]
+};
+
 document.addEventListener("keypress", (e) => {
     if (e.code === 'Enter' || e.code === 'Space') {
         scaleImgWrapper.style.display = "none";
